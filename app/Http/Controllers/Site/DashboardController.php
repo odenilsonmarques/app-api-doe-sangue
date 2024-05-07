1bdce49e435571c82819e7ff24491a6d7665182a
odenilson marques <odmarquesgm@gmail.com>
2024-05-07 16:11:38 -0300

    update dashboard

--- app/Http/Controllers/Site/DashboardController.php
+++ app/Http/Controllers/Site/DashboardController.php
@@ -11,43 +11,41 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-     // get only scheduling user logeded
-     public function index()
-     {
-        
-             $user = Auth::user(); // Obtenha o usuário logado
- 
-             // Agora, em vez de buscar todos os agendamentos, filtre-os com base no ID do usuário
-            //  $schedulings = Scheduling::where('user_id', $user->id)->get();
-            $schedulings = Scheduling::with('bloodCenter')->where('user_id', $user->id)->get();
+    // get only scheduling user logeded
+    public function index()
+    {
 
-            $historrics = Historic::with(['user', 'scheduling', 'bloodCenter'])->where('user_id', $user->id)->get();
+        $user = Auth::user(); // Obtenha o usuário logado
+
+        // Agora, em vez de buscar todos os agendamentos, filtre-os com base no ID do usuário
+        //  $schedulings = Scheduling::where('user_id', $user->id)->get();
+        $schedulings = Scheduling::with('bloodCenter')->where('user_id', $user->id)->get();
+
+        $historrics = Historic::with(['user', 'scheduling', 'bloodCenter'])->where('user_id', $user->id)->get();
 
 
-      
 
-            $schedulingAlls = Scheduling::with('user', 'bloodCenter')->get();
+
+        $schedulingAlls = Scheduling::with('user', 'bloodCenter')->get();
         // $schedulingAlls = Scheduling::with('user', 'bloodCenter')->latest()->get();
 
 
-            //  dd($historrics);
- 
-             // return response()->json(['schedulings' => $schedulings], 200);
-             return view('dashboard.index', [
-                'schedulings' => $schedulings,
-                'historics' => $historrics,
-                'schedulingAlls' => $schedulingAlls
-             ]);
-
-            
+        $total_quantity_blood = Historic::sum('quantity_blood');
 
 
+        $bloodCentersWithQuantity = BloodCenter::withSum('historics', 'quantity_blood')->get();
 
 
+        //  dd($historrics);
 
+        // return response()->json(['schedulings' => $schedulings], 200);
+        return view('dashboard.index', [
+            'schedulings' => $schedulings,
+            'historics' => $historrics,
+            'schedulingAlls' => $schedulingAlls,
+            'total_quantity_blood' => $total_quantity_blood,
+            'bloodCentersWithQuantity' => $bloodCentersWithQuantity
 
-
-
-    
-     }
+        ]);
+    }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,26 +5,54 @@
     <div class="container-fluid">
         <div class="container">
             @if (auth()->user()->is_admin)
+                <div class="row ">
+                    <div class="col-lg-6 shadow p-3 mb-5 bg-body rounded my-5 text-center">
+                        <h3 class="mt-5 text-center">Total de bolsas doadas</h3>
+                        <p class="my-5">{{ $total_quantity_blood }} ML</p>
+                        <p>Atualizado em: {{ \Carbon\Carbon::now()->format('d/m/Y') }}</p>
+                    </div>
+
+                    <div class="col-lg-6 shadow p-3 mb-5 bg-body rounded my-5">
+                        <h3 class="mt-5 text-center">Total de bolsas por hemocentro</h3>
+                        @foreach ($bloodCentersWithQuantity as $bloodCenter)
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h5>{{ $bloodCenter->name }}</h5>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>Total : {{ $bloodCenter->historics_sum_quantity_blood }} ML</p> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    
+                </div>
+
                 <div class="row">
-                    <div class="col lg 6">
+                    <div class="col-lg-12">
                         <h3 class="mt-5 text-center">Agendamentos</h3>
                         <table class="table table-striped">
                             <thead>
                                 <tr>
-                                    <th scope="col">Data</th>
-                                    <th scope="col">Hemocentro</th>
-                                    <th scope="col">Sangue</th>
                                     <th scope="col">Doador</th>
-
+                                    <th scope="col">Telefone</th>
+                                    <th scope="col">Data</th>
+                                    <th scope="col">Tipo sanguineo</th>
+                                    <th scope="col">Hemocentro</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($schedulingAlls as $schedulingAll)
                                     <tr>
-                                        <td>{{ date('d/m/Y', strtotime($schedulingAll->scheduling_date)) }}</td>
-                                        <td>{{ $schedulingAll->bloodCenter->name }}</td>
-                                        <td>{{ $schedulingAll->blood_type }}</td>
                                         <td>{{ $schedulingAll->user->name }}</td>
+                                        <td>{{ $schedulingAll->phone }}</td>
+                                        <td>{{ date('d/m/Y', strtotime($schedulingAll->scheduling_date)) }}</td>
+                                        <td>{{ $schedulingAll->blood_type }}</td>
+                                        <td>{{ $schedulingAll->bloodCenter->name }}</td>
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -32,11 +60,6 @@
                     </div>
                 </div>
             @else
-
-
-
-
-            
                 <div class="row">
                     <div class="col-lg-6 my-5">
                         <a href="{{ route('schedulings.create') }}" class="my-3 btn btn-link">Realizar novo agendamento</a>
